8349fca1c93a047ffccfc87d1392c4f2299f61a2
JobLessGod <iamjoblessgod@gmail.com>
2024-11-10 22:50:57 +0545

    making navigation

---
+++ src/components/NavBar.js
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { ModeToggle } from "./modeToggle";
+
+const NavBar = () => {
+  return (
+    <nav className="p-4 bg-background/50 sticky top-0 border-b-2 backdrop-blur-[3px] z-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">
+          <Link href="/">Jeb Dhili</Link>
+        </div>
+        <ul className="hidden lg:flex space-x-4 items-center">
+          <li>
+            <Link
+              href={"/"}
+              className="hover:text-gray-400 transition-transform duration-300"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/developer"}
+              className="hover:text-gray-400 transition-transform duration-300"
+            >
+              Developer
+            </Link>
+          </li>
+          <li>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </li>
+          <li>
+            <ModeToggle/>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
